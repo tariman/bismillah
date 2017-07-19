9ab05828802a5f45120772a1182a404deffbe816
tariman <tariman888@gmail.com>
2017-07-19 08:00:28 +0700

    versi 1.0.0

---
+++ index.php
@@ -0,0 +1,67 @@
+     <!DOCTYPE HTML PUBLIC>  
+    <html>  
+    <head>  
+    <title>Kerangka Web</title>  
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
+    <link href="style.css" rel="stylesheet" type="text/css"> 
+    </head>  
+      
+    <body>  
+    <table width="900" border="0" align="center" cellpadding="0" cellspacing="0">  
+      <tr bgcolor="gray">  
+        <td height="100" colspan="2"></td>  
+      </tr>  
+      <tr bgcolor="black">  
+        <td colspan="2"><marquee onmouseover="this.stop()" onmouseout="this.start()" truespeed="true" scrollamount="1" scrolldelay="40" direction="left">  
+        <strong><div id="login">Selamat Datang Di Website Kami, Website Ini Masih Dalam Tahap Pembuatan</div></strong>  
+    </marquee>  
+
+
+    </td>  
+      </tr>  
+      <tr>  
+        <td width="200" height="600" valign="top" bgcolor="gray"><table width="200" border="0" cellspacing="0" cellpadding="0">  
+          <tr>  
+            <td><form name="form1" method="post" action="cek_login.php">  
+              <table width="200" border="0" cellspacing="0" cellpadding="2">  
+                <tr bgcolor="#003366">  
+                  <td height="30" colspan="2"><div align="center"><div id="login"><strong>Login Admin </strong></div></div></td>  
+                  </tr>  
+                <tr>  
+                  <td width="74" height="30"><b>Username</b></td>  
+                  <td width="126">:   
+                    <input name="username" type="text" id="username" size="10"></td>  
+                </tr>  
+                <tr>  
+                  <td height="30"><b>Password</b></td>  
+                  <td>:   
+                    <input name="password" type="password" id="password" size="10"></td>  
+                </tr>  
+                <tr>  
+                  <td height="30">&nbsp;</td>  
+                  <td><input type="submit" name="Submit" value="Login"></td> 
+                </tr>  
+              </table>  
+            </form>
+           </td>  
+          </tr>  
+          <tr>  
+            <td><div id="kiri">
+    <div id="login">
+    <p><center><b>PROFIL</center></p> 
+    <p>Nama : Tariman </p>
+    <p>Email   : tariman888@gmail.com</p></b>
+    </div>
+    </div>
+    </td>  
+          </tr>  
+        </table> 
+    </td>   
+        <td valign="top">&nbsp;</td>  
+      </tr>  
+      <tr bgcolor="black">  
+        <td height="80" colspan="2"><div id="login"><center>Copyright 2017 <br>Design : Tariman Oi </div></td>  
+      </tr>  
+    </table>  
+    </body>  
+    </html>  
